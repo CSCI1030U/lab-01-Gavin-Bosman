bda6520e360cf5562ca2d2ce3341b234180ba4ee
gavinbos <gavin.bosman@ontariotechu.net>
2025-09-10 13:41:49 -0400

    lab01 tests completed

--- lab01.py
+++ lab01.py
@@ -1,3 +1,5 @@
+# Gavin Bosman 100781902
+
 def main():
     # YOUR CODE FOR PART 1 GOES HERE 
     cost_per_item = 19.99   # floating point number
@@ -6,10 +8,12 @@ def main():
     tax = subtotal_cost * 0.13
     total_cost = subtotal_cost + tax
 
-
     # YOUR CODE FOR PART 2 GOES HERE
     print(f'cost_per_item = ${cost_per_item:0.2f}') # a sample for you to use for the other prices
-    print(f'quantity = ${quantity:0.2f}')
+    print(f"quantity = {quantity}")
+    print(f'subtotal_cost = ${subtotal_cost:0.2f}')
+    print(f'tax = ${tax:0.2f}')
+    print(f"total_cost = ${total_cost:0.2f}")
 
     # THIS IS THE CODE FOR PART 3
     initial_investment = 1000
@@ -20,8 +24,10 @@ def main():
     investment += investment * interest_rate
     investment += investment * interest_rate
     investment += investment * interest_rate
-    print('After 5 years, your investment will be worth ' + investment + ' dollars.')
-    # expected output: After 5 years, your investment will be worth 1187.6863056468749 dollars.
+    print('After 5 years, your investment will be worth ' + str(investment) + ' dollars.')
+
+    # # expected output: After 5 years, your investment will be worth 1187.6863056468749 dollars.
+    
 
 if __name__ == "__main__":
     main()
